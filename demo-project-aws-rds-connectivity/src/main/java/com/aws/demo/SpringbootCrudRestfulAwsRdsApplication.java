caa6ce36d1a2a1bc7622f3e5b85422ebfab5a062
ghanshyam shukla <gsgmca682@gmail.com>
2023-09-28 08:05:22 -0700

    code committed for the aws rds connectivity

---
+++ demo-project-aws-rds-connectivity/src/main/java/com/aws/demo/SpringbootCrudRestfulAwsRdsApplication.java
@@ -0,0 +1,13 @@
+package com.aws.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class SpringbootCrudRestfulAwsRdsApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
