40cb3d796bea8ee192dd56b2db02d0900b4b68b4
ghanshyam shukla <gsgmca682@gmail.com>
2023-11-25 09:07:02 -0800

    code committed for the java cdk

---
+++ demo-project-amazon-cdk-demo/src/main/java/org/example/aws/demo/App.java
@@ -0,0 +1,18 @@
+package org.example.aws.demo;
+
+import software.amazon.awscdk.services.s3.CfnBucket;
+import software.amazon.jsii.JsiiObjectRef;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       // CfnBucket cfn=new CfnBucket(JsiiObjectRef.fromObjId("ghans"));
+
+        System.out.println( "Hello World!" );
+    }
+}
